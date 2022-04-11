23bbcc2ac0eaf44751d326852e5ceb11419cb2ce
Sylvia <98797671+sylviaadrastea@users.noreply.github.com>
2022-04-11 14:33:04 +0700

    Update spamchat.js

--- plugins/spamchat.js
+++ plugins/spamchat.js
@@ -41,7 +41,7 @@ handler.tags = ['spam']
 handler.command = /^(spamchat)$/i
 handler.owner = false
 handler.mods = false
-handler.premium = false
+handler.premium = true
 handler.group = false
 handler.private = false
 
