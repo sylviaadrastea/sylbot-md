b4d9d8519d72d8f144b8c1e44db6dd3d9dcd100d
Sylvia <98797671+sylviaadrastea@users.noreply.github.com>
2022-04-16 13:58:42 +0700

    Update stickerwa.js

--- plugins/stickerwa.js
+++ plugins/stickerwa.js
@@ -4,7 +4,7 @@ let { sticker } = require('../lib/sticker')
 let handler = async (m, { conn, text }) => {
    if (!text) throw 'Masukan pencarian, Contoh *#stickerwa pentol*'
  
-  let res = await fetch(`https://xteam.xyz/sticker/stickerly?q=${text}&APIKEY=MIMINETBOT`)
+  let res = await fetch(`https://api.xteam.xyz/sticker/stickerly?q=${text}&APIKEY=1ffe5ec4414fd000`)
   let json = await res.json()
   for (let i of json.result.stickerlist) {
 conn.sendMessage(m.chat, await sticker(false, i, packname, author), 'stickerMessage', { quoted: m })
